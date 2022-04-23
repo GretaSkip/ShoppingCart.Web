57f02483e102e205e4e32d487c055b339df282fa
Greta Skipare <gretchfurlined@gmail.com>
2022-04-23 16:40:28 +0300

    Category Controller
    Main functions completed

--- ShoppingCart.Web/Areas/Admin/Controllers/Category.cs
+++ ShoppingCart.Web/Areas/Admin/Controllers/Category.cs
@@ -22,6 +22,81 @@ namespace ShoppingCart.Web.Areas.Admin.Controllers
 
         }
 
+        [HttpGet]
+        public IActionResult CreateUpdate(int? id)
+        {
+            CategoryVM vm = new CategoryVM();
+            if (id == null || id == 0)
+            {
+                return View(vm);
+            }
+            else
+            {
+                vm.Category = _unitofWork.Category.GetT(x => x.Id == id);
+                if (vm.Category == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(vm);
+                }
 
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUpdate(CategoryVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+                if (vm.Category.Id == 0)
+                {
+                    _unitofWork.Category.Add(vm.Category);
+                    TempData["success"] = "Category Creation Done!";
+                }
+                else
+                {
+                    _unitofWork.Category.Update(vm.Category);
+                    TempData["success"] = "Category Update Done!";
+                }
+
+                _unitofWork.Save();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _unitofWork.Category.GetT(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteData(int? id)
+        {
+            var category = _unitofWork.Category.GetT(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _unitofWork.Category.Delete(category);
+            _unitofWork.Save();
+            TempData["success"] = "Category Deleted Done!";
+            return RedirectToAction("Index");
+        }
     }
 }
